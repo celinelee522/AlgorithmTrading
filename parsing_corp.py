e06bb38f0a166397b5f98e6bffd6ce3f5165c856
celinelee522 <celinelee522@gmail.com>
2017-10-04 22:52:10 +0900

    computing mktCap growth rate 1,2,3 years after

--- parsing_corp.py
+++ parsing_corp.py
@@ -10,22 +10,22 @@ def checkExist(date, corpName) :
             return True
     return False
 
-def findDate(date, corpName, count) : # should return log(mark cap)
+def findDate(date, corpName) : # should return log(mark cap)
     date = int(date)
-    for list in companies[corpName] :
-        if (date == list[0]) :
-            print("List of targeted date : %s" %list)
-            return list[1]
-    date += 1
-    count += 1
-    if (count < 5) :
-        print("recursive call")
-        findDate(date, corpName, count)
-    print("not found...")
+
+    count = 0
+    while count < 3 :
+        for list in companies[corpName] :
+            if (date == int(list[0])) :
+                print("List of targeted date : %s" %list)
+                return list[1]
+        count += 1
+        date += 1
+
 
 def computeGrowth(base, yearsLater):
     if yearsLater != None :
-        return (yearsLater/base - 1) * 100
+        return (yearsLater/base - 1) * 100.0
     else :
         return 0
 
@@ -50,7 +50,7 @@ labels.append(firstLine[5])
 
 #print(labels)
 
-companies = {}
+companies = {} #dictionary
 
 for row in in_file:
     row = row.strip()
@@ -67,8 +67,8 @@ for row in in_file:
         row[2] = int(float(row[2])) # share of outstanding
         row[3] = int(float(row[3])) # share price
         if (row[2]*row[3] > 0):
-            marketCap = round( log10(row[2]*row[3]), 2 )
-            stock.append(marketCap) # ln(market capitalization)
+            mktCap = round( log10(row[2]*row[3]), 2 )
+            stock.append(mktCap) # ln(market capitalization)
             # stock = [Date, Corp name, ln(market cap)]
             #company = stock[1]
 
@@ -80,45 +80,50 @@ for row in in_file:
 # finished making up dictionary(key == company name). 
 # values inside key are bunch of lists [date, log(market Cap)]
 
-for company in companies : 
-    for list in companies[company] :
-        print(list)
-        date = list[0]
-        marketCap = list[1]
-        oneYearLater = int(date) + 10000
-        twoYearLater = int(date) + 20000
-        threeYearLater = int(date) + 30000
 
-        oneYMarCap = findDate(oneYearLater, company, 0)
-        list.append(oneYMarCap)
-        list.append(int(computeGrowth(marketCap, oneYMarCap)))
-        print("one year later : %s, market cap : %s " %(oneYearLater, oneYMarCap))
-
-        twoYMarCap = findDate(twoYearLater, company, 0)
-        list.append(twoYMarCap)
-        list.append(int(computeGrowth(marketCap, twoYearLater)))
-        print("two year later : %s, market cap : %s " %(twoYearLater, twoYMarCap))
-
-        threeYMarCap = findDate(twoYearLater, company, 0)
-        list.append(threeYMarCap)
-        list.append(int(computeGrowth(marketCap, threeYMarCap)))
-        print("three year later : %s, market cap : %s " %(threeYearLater, threeYMarCap))
-
-
-"""
-
-sorted(dateDic.keys())
 f = open("newStockList.txt", 'w')
-for date in dateDic:
-	f.write(date + "\n")
-	for item in dateDic[date] :
-	    f.write('{:30}'.format(item[0]) + '{:10.2f}'.format(item[1]) + "\n") #compName, marketCapitalization
-        f.write('{:7.2f}'.format(float(item[2])) + '(' + '{:2}'.format(item[3]) + ')') # oneYearAfter, growthRate
-        f.write('{:7.2f}'.formate(float(item[4])) + '(' + '{:2}'.format(item[5]) + ')')
-        f.write('{:7.2f}'.formate(float(item[7])) + '(' + '{:2}'.format(item[8]) + ')' + "\n")
+for company in companies : 
+    if company == 'DAEWOO ENGINEERING CONSTRUC' :
+        f.write('DAEWOO ENGINEERING CONSTRUC\n')
+        for row in companies[company] :
+            list = [None] * 9
+            list[0] = row[0] # date
+            list[1] = row[1] # market capitalization
+            oneYearLater = int(list[0]) + 10000
+            twoYearLater = int(list[0]) + 20000
+            threeYearLater = int(list[0]) + 30000
+
+            mktCap_one = findDate(oneYearLater, company)
+            list[2] = mktCap_one
+            list[3] = computeGrowth(list[1], mktCap_one) # growth rate a year after
+            grthRate_one = int(computeGrowth(list[1], mktCap_one))
+
+            mktCap_two = findDate(twoYearLater, company)
+            list[4] = mktCap_two
+            list[5] = computeGrowth(list[1], mktCap_two) # growth rate two years after
+            grthRate_two = int(computeGrowth(list[1], mktCap_two))
+
+            mktCap_three = findDate(twoYearLater, company)
+            list[6] = mktCap_three
+            list[7] = computeGrowth(list[1], mktCap_three) # growth rate three years after
+            grthRate_three = int(computeGrowth(list[1], mktCap_three))
+            print("<base year: %s> 1 year later market cap : %s( %f)/ 2 years later market cap : %s( %f)/ 3 years later market cap : %s( %f)" %(list[0], mktCap_one, grthRate_one, mktCap_two, grthRate_two, mktCap_three, grthRate_three))
+
+            sorted(companies.keys())
+            
+            f.write('{:10}'.format(list[0]) + '{:10.2f}'.format(list[1])) #date, marketCapitalization
+            #if (list[2] is not None and list[3] is not None and list[4] is not None and list[5] is not None and list[6] is not None and list[7] is not None) :
+            for i in range(1,4) :
+                i = 2*i
+                print("i : %d" %i)
+                if (list[i] is not None) :
+                    f.write('{:7.2f}'.format(float(list[i])) + '(' + '{:3.1f}'.format(list[i+1]) + ')')
+                else :
+                    f.write('  None(None)')
+            f.write('\n')
+
 f.close()
 
-"""
 
 
 
