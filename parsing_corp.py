62db261fd56ec6994c0d965e0b64f7a7197cfaab
celinelee522 <celinelee522@gmail.com>
2017-10-05 12:55:59 +0900

    typo

--- parsing_corp.py
+++ parsing_corp.py
@@ -77,44 +77,43 @@ for row in in_file:
 
 f = open("newStockList.txt", 'w')
 for company in companies : 
-    if company == 'DAEWOO ENGINEERING CONSTRUC' :
-        f.write('DAEWOO ENGINEERING CONSTRUC\n')
-        for row in companies[company] :
-            list = [None] * 9
-            list[0] = row[0] # date
-            list[1] = row[1] # market capitalization
-            oneYearLater = int(list[0]) + 10000
-            twoYearLater = int(list[0]) + 20000
-            threeYearLater = int(list[0]) + 30000
+    #if company == 'DAEWOO ENGINEERING CONSTRUC' :
+    f.write('%s\n' %company)
+    for row in companies[company] :
+        list = [None] * 9
+        list[0] = row[0] # date
+        list[1] = row[1] # market capitalization
+        oneYearLater = int(list[0]) + 10000
+        twoYearLater = int(list[0]) + 20000
+        threeYearLater = int(list[0]) + 30000
 
-            mktCap_one = findDate(oneYearLater, company)
-            list[2] = mktCap_one
-            list[3] = computeGrowth(list[1], mktCap_one) # growth rate a year after
-            grthRate_one = int(computeGrowth(list[1], mktCap_one))
+        mktCap_one = findDate(oneYearLater, company)
+        list[2] = mktCap_one
+        list[3] = computeGrowth(list[1], mktCap_one) # growth rate a year after
+        grthRate_one = int(computeGrowth(list[1], mktCap_one))
 
-            mktCap_two = findDate(twoYearLater, company)
-            list[4] = mktCap_two
-            list[5] = computeGrowth(list[1], mktCap_two) # growth rate two years after
-            grthRate_two = int(computeGrowth(list[1], mktCap_two))
+        mktCap_two = findDate(twoYearLater, company)
+        list[4] = mktCap_two
+        list[5] = computeGrowth(list[1], mktCap_two) # growth rate two years after
+        grthRate_two = int(computeGrowth(list[1], mktCap_two))
 
-            mktCap_three = findDate(twoYearLater, company)
-            list[6] = mktCap_three
-            list[7] = computeGrowth(list[1], mktCap_three) # growth rate three years after
-            grthRate_three = int(computeGrowth(list[1], mktCap_three))
-            print("<base year: %s> 1 year later market cap : %s( %f)/ 2 years later market cap : %s( %f)/ 3 years later market cap : %s( %f)" %(list[0], mktCap_one, grthRate_one, mktCap_two, grthRate_two, mktCap_three, grthRate_three))
+        mktCap_three = findDate(threeYearLater, company)
+        list[6] = mktCap_three
+        list[7] = computeGrowth(list[1], mktCap_three) # growth rate three years after
+        grthRate_three = int(computeGrowth(list[1], mktCap_three))
+        print("<base year: %s> 1 year later market cap : %s( %f)/ 2 years later market cap : %s( %f)/ 3 years later market cap : %s( %f)" %(list[0], mktCap_one, grthRate_one, mktCap_two, grthRate_two, mktCap_three, grthRate_three))
 
-            sorted(companies.keys())
-            
-            f.write('{:10}'.format(list[0]) + '{:10.2f}  '.format(list[1])) #date, marketCapitalization
-            #if (list[2] is not None and list[3] is not None and list[4] is not None and list[5] is not None and list[6] is not None and list[7] is not None) :
-            for i in range(1,4) :
-                i = 2*i
-                print("i : %d" %i)
-                if (list[i] is not None) :
-                    f.write('{:7.2f}'.format(float(list[i])) + '(' + '{:3.1f}'.format(list[i+1]) + ')')
-                else :
-                    f.write('  None(None)')
-            f.write('\n')
+        sorted(companies.keys())
+        
+        f.write('{:10}'.format(list[0]) + '{:10.2f}  '.format(list[1])) #date, marketCapitalization
+        #if (list[2] is not None and list[3] is not None and list[4] is not None and list[5] is not None and list[6] is not None and list[7] is not None) :
+        for i in range(1,4) :
+            i = 2*i
+            if (list[i] is not None) :
+                f.write('{:7.2f}'.format(float(list[i])) + '(' + '{:3.1f}'.format(list[i+1]) + ')')
+            else :
+                f.write('   None(None)')
+        f.write('\n')
 
 f.close()
 
